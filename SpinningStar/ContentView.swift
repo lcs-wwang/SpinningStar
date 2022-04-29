28306d645cea98dcad7676d7fdfeac22c5725df4
winstonwang <wwang@lcs.on.ca>
2022-04-29 10:06:40 -0400

    unfilled star now disappears

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
     //1. initial state of rotation for the star
     @State var starRotation = 0.0
     
+    //1. initial opacity for unfilled star
+    @State var unfilledStarOpacity = 1.0
+    
+    
     //MARK: computed properties
     var body: some View {
         Image(systemName: "star")
@@ -21,6 +25,7 @@ struct ContentView: View {
             .frame(width: 40, height: 40)
             .foregroundColor(.yellow)
             .rotation3DEffect(.degrees(starRotation), axis: (x: 0, y: 1, z: 0))
+            .opacity(unfilledStarOpacity)
         //trigger the state change with a tap
             .onTapGesture {
                 withAnimation(
@@ -29,6 +34,9 @@ struct ContentView: View {
                 ) {
                     //3. rotate the star twice (change rotation state)
                     starRotation += 360.0 * 2
+                    
+                    //desired end opacity for unfilled star
+                    unfilledStarOpacity = 0.0
                 }
             }
          
