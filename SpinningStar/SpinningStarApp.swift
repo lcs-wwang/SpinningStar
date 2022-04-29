c0acb6b54b8b5e1671614b4d68cedc3ddc2cb92b
winstonwang <wwang@lcs.on.ca>
2022-04-29 09:46:58 -0400

    Initial Commit

---
+++ SpinningStar/SpinningStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinningStarApp.swift
+//  SpinningStar
+//
+//  Created by Winston Wang on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct SpinningStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
